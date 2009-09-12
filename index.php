9989d4f401dd81add853887068a5b9dc56b8daab
andrew-waters <andrew@band-x-media.co.uk>
2009-09-12 17:28:09 +0000

    Started Frontend Development with the addition of a behaviour and observer call. Added empty function serveFile() which will do the magic on the frontend
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/downloads@24 7774936b-a2d8-42ea-a4f4-ad82cefe4934

--- index.php
+++ index.php
@@ -11,67 +11,22 @@
 	include('models/CategoryManager.php');
 	include('models/HistoryManager.php');
 	include('models/SettingsManager.php');
+	include('models/FrontendManager.php');
 
 	Plugin::addController('downloads', 'Downloads', 'developer,administrator,editor', TRUE);
 
+	Behavior::add('download_page', '');
+	Observer::observe('page_found', 'downloads_page_found');
 
-
-function downloaddoc() {
-
-	global $__CMS_CONN__;
-	$id = $_GET['id'];
-	$referer = $_SERVER['HTTP_REFERER'];
-	
-	if (empty($id)) {
-		header("Location: ".$referer."");
-	}
-	
-	else {
-	
-		$downloads = "SELECT * FROM ".TABLE_PREFIX."downloads WHERE id='$id'";
-		$downloads = $__CMS_CONN__->prepare($downloads);
-		$downloads->execute();
-		
-		while ($download = $downloads->fetchObject()) {
-			$name		= $download->name;
-			$published	= $download->published;
-			$downloads	= $download->downloads;
-			$extension	= $download->extension;
-	
-			$name		= str_replace(' ', '_', $name);
-			$name		= str_replace('\'', '_', $name);
-			$name		= str_replace('/', '-', $name);
-			$name		= str_replace(':', '_', $name);
-			$name		= str_replace('  ', '_', $name);
-			$name		= str_replace('"', '_', $name);
-			$name		= str_replace(';', '_', $name);
-			$name		= strtolower($name);
-	
-			if ($published == 1) {
-
-				$newcount	= $downloads + 1;
-				$update		= "UPDATE ".TABLE_PREFIX."downloads SET downloads='".$newcount."' WHERE id='".$id."'";
-				$update		= $__CMS_CONN__->prepare($update);
-				$update->execute();
-
-				$current_dir			= DOWNLOADS_DIRECTORY . '/';
-				$current_dir			= str_replace('//', '/', $current_dir);
-				$current_file			= $current_dir . $id . $extension ;
-				$target_file			= $current_dir . 'cache/' . $name . $extension;
-
-				if(copy($current_file, $target_file)) {
-					header('Location: '.URL_DOWNLOADS.'cache/'.$name.''.$extension.'');
-				}
-
-				else {
-					echo $current_file;
-					echo '<br />';
-					echo $target_file;
-				}
-			}
-			else {
-				header('Location: '.URL_PUBLIC.'download/unavailable');
+	function downloads_page_found($page) {
+		global $__CMS_CONN__;
+		$sql = "SELECT * FROM ".TABLE_PREFIX."page WHERE behavior_id='download_page'";
+		$stmt = $__CMS_CONN__->prepare($sql);
+		$stmt->execute();
+		while($pages = $stmt->fetchObject()) {
+			if($page->slug == $pages->slug) {
+				$frontendManager = new DownloadFrontendManager();
+				$serveFile = $frontendManager->serveFile();
 			}
 		}
 	}
-}
---
+++ models/FrontendManager.php
@@ -0,0 +1,14 @@
+<?php
+
+class DownloadFrontendManager {
+
+	function __construct() {
+		global $__CMS_CONN__;
+		$this->db = $__CMS_CONN__;
+	}
+
+	function serveFile() {
+		
+	}
+
+}
