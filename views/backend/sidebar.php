fd80b1d9d67880220eff3507bf7ca0dcbd025505
andrew-waters <andrew@band-x-media.co.uk>
2009-09-13 13:32:22 +0000

    Added History Icon and nav element to sidebar
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/downloads@32 7774936b-a2d8-42ea-a4f4-ad82cefe4934

--- views/backend/sidebar.php
+++ views/backend/sidebar.php
@@ -4,6 +4,7 @@ if (Dispatcher::getAction() != 'view'):
 ?>
 <p class="button"><a href="<?php echo get_url('plugin/downloads/'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download.png" align="middle" alt="Downloads" /> Downloads</a></p>
 <p class="button"><a href="<?php echo get_url('plugin/downloads/files/add'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download-add.png" align="middle" alt="Add a Download" /> Add Download</a></p>
+<p class="button"><a href="<?php echo get_url('plugin/downloads/history'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download-history.png" align="middle" alt="History" /> History</a></p>
 <p class="button"><a href="<?php echo get_url('plugin/downloads/categories'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download-categories.png" align="middle" alt="Download Categories" /> Categories</a></p>
 <p class="button"><a href="<?php echo get_url('plugin/downloads/categories/add'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download-categories-add.png" align="middle" alt="Add a Download Category" /> Add Category</a></p>
 <p class="button"><a href="<?php echo get_url('plugin/downloads/settings'); ?>"><img src="../<?php echo $settings['core_root']; ?>/plugins/downloads/images/download-settings.png" align="middle" alt="Download Settings" /> Settings</a></p>
