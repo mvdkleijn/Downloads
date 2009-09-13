dd49dfa4bf90c387f4332153382ed905e15c1d4b
andrew-waters <andrew@band-x-media.co.uk>
2009-09-13 13:52:30 +0000

    Added quick link to history from the download edit page
    git-svn-id: file://localhost/Users/Andrew/Desktop/cvs/svn-repos/downloads@34 7774936b-a2d8-42ea-a4f4-ad82cefe4934

--- views/backend/downloads.php
+++ views/backend/downloads.php
@@ -21,6 +21,7 @@
 
 	if(is_numeric($id)) {
 		echo '<p>Added by <a href="'.get_url('user/edit/'.$download['added_by_id'].'').'">'.$download['added_by_name'].'</a> on '.date('dS F, Y', $download['date_added']).'';
+		echo '<p>Would you like to see the <a href="'.get_url('plugin/downloads/history/'.$download['added_by_id'].'').'">history for this file</a>?</p>';
 		if($download['date_publish'] != '0') {
 			$publishDate		= date('Y-m-d', $download['date_publish']);
 			$publishTime		= date('G:i', $download['date_publish']);
